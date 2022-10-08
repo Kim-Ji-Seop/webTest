0d37de9baa9650358047a2ab2a61a6ab1ec233d2
Kim-Ji-Seop <76149358+Kim-Ji-Seop@users.noreply.github.com>
2022-10-09 00:45:41 +0900

    2022.10.09 - mustache 게시판 렌더링 화면 구현 & 조회/수정/삭제 구현

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,71 @@
+var main = {
+    init  : function () {
+        var _this = this;
+        $('#btn-save').on('click',function (){ // 게시글 등록 버튼
+           _this.save();
+        });
+        $('#btn-update').on('click',function (){ // 게시글 수정 버튼
+            _this.update(); // btn-update란 id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 update function을 실행하도록 이벤트를 등록한다.
+        });
+        $('#btn-delete').on('click',function (){ // 게시글 수정 버튼
+            _this.delete(); // btn-update란 id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 update function을 실행하도록 이벤트를 등록한다.
+        });
+    },
+    save : function () {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type:'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 등록되었습니다.');
+            window.location.href='/'; // 글 등록이 성공하면 메인페이지(/)로 이동한다.
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    update : function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/'+id, // 어느 게시글을 수정할지 URL Path로 구분하기 위해 Path에 id를 추가합니다.
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+main.init();
